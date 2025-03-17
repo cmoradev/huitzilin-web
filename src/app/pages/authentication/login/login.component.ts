4e925af7145e44387508a36eb837ec72cf38d7cb
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-03-17 12:06:01 -0500

    feat: add Tailwind CSS and custom theming for Angular Material

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCard, MatCardContent],
+  imports: [MatCard, MatCardContent, MatFormField, MatInput, MatButton],
   templateUrl: './login.component.html',
   styles: ``,
 })
