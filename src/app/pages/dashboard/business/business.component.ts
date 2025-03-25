d5f0835d46a1b8ba8455bae81d29a66cbfcc311c
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-03-25 14:18:42 -0500

    feat: enhance business component with search functionality and loading state

--- src/app/pages/dashboard/business/business.component.ts
+++ src/app/pages/dashboard/business/business.component.ts
@@ -1,4 +1,12 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatIconButton } from '@angular/material/button';
 import {
   MatCard,
@@ -6,6 +14,7 @@ import {
   MatCardHeader,
   MatCardTitle,
 } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 import {
   MatFormField,
   MatLabel,
@@ -13,15 +22,19 @@ import {
 } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTooltip } from '@angular/material/tooltip';
-import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
-import { CompanyPartsFragment, GetCompaniesPageGQL } from '@graphql';
-import { CompanyFormDialogComponent } from './company-form-dialog/company-form-dialog.component';
-import { CompanyDeleteDialogComponent } from './company-delete-dialog/company-delete-dialog.component';
 import { AvatarComponent } from '@components/avatar/avatar.component';
-
+import {
+  CompanyFilter,
+  CompanyPartsFragment,
+  GetCompaniesPageGQL,
+} from '@graphql';
+import { debounceTime, merge, startWith } from 'rxjs';
+import { CompanyDeleteDialogComponent } from './company-delete-dialog/company-delete-dialog.component';
+import { CompanyFormDialogComponent } from './company-form-dialog/company-form-dialog.component';
 
 @Component({
   selector: 'app-business',
@@ -39,23 +52,34 @@ import { AvatarComponent } from '@components/avatar/avatar.component';
     MatTooltip,
     MatTableModule,
     MatPaginator,
-    AvatarComponent
+    AvatarComponent,
+    ReactiveFormsModule,
+    MatProgressBar,
   ],
   templateUrl: './business.component.html',
   styles: ``,
 })
-export class BusinessComponent implements OnInit {
+export class BusinessComponent implements AfterViewInit {
+  @ViewChild('paginator') public paginator!: MatPaginator;
+  public searchControl = new FormControl('');
+
   public displayedColumns: string[] = ['name', 'address', 'actions'];
   public dataSource = new MatTableDataSource<CompanyPartsFragment>([]);
 
   public loading = signal(false);
+  public totalCount = signal(0);
 
   private readonly dialog = inject(MatDialog);
   private readonly _companiesPageGQL = inject(GetCompaniesPageGQL);
 
-
-  ngOnInit(): void {
-    this._fetch();
+  ngAfterViewInit(): void {
+    merge(this.paginator.page, this.searchControl.valueChanges)
+      .pipe(debounceTime(300), startWith({}))
+      .subscribe({
+        next: () => {
+          this.refresh();
+        },
+      });
   }
 
   public openCreateDialog(): void {
@@ -66,7 +90,7 @@ export class BusinessComponent implements OnInit {
     $dialog.afterClosed().subscribe({
       next: (company) => {
         if (company) {
-          this._fetch();
+          this.refresh();
         }
       },
     });
@@ -81,7 +105,7 @@ export class BusinessComponent implements OnInit {
     $dialog.afterClosed().subscribe({
       next: (company) => {
         if (company) {
-          this._fetch();
+          this.refresh();
         }
       },
     });
@@ -95,16 +119,23 @@ export class BusinessComponent implements OnInit {
     $dialog.afterClosed().subscribe({
       next: (company) => {
         if (company) {
-          this._fetch();
+          this.refresh();
         }
       },
     });
   }
 
-  private _fetch() {
+  public refresh() {
+    const limit: number = this.paginator.pageSize;
+    const offset: number = this.paginator.pageIndex * limit;
+
+    const filter: CompanyFilter = {
+      name: { iLike: `%${this.searchControl.value}%` },
+    };
+
     this._companiesPageGQL
       .watch(
-        {},
+        { limit, offset, filter },
         {
           fetchPolicy: 'cache-and-network',
           nextFetchPolicy: 'cache-and-network',
@@ -117,6 +148,7 @@ export class BusinessComponent implements OnInit {
           this.dataSource.data = nodes;
 
           this.loading.set(loading);
+          this.totalCount.set(totalCount);
         },
       });
   }
