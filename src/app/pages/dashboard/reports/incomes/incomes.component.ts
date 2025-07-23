8f14184a02af3a8de7ae62df152a2c63b9bafc65
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-07-23 09:16:51 -0500

    feat(incomes): implement income report layout with date range filter and refresh functionality

--- src/app/pages/dashboard/reports/incomes/incomes.component.ts
+++ src/app/pages/dashboard/reports/incomes/incomes.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-incomes',
-  imports: [],
+  imports: [
+    NgClass,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatTooltipModule,
+  ],
   templateUrl: './incomes.component.html',
-  styles: ``
+  styles: ``,
 })
 export class IncomesComponent {
+  public loading = signal<boolean>(false);
+  public totalCount = signal<number>(0);
+  public displayedColumns: string[] = ['name', 'actions'];
+  public dataSource = new MatTableDataSource<any>([]);
+
+  public refresh() {
+    this.loading.set(true);
+    // Simulate an API call
+  }
 
 }
