7c4775f7264e43c44708b9dd9ecd4381328fb5d8
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-07-25 09:23:06 -0500

    feat(incomes): add ECharts integration for income visualization and update total display

--- src/app/pages/dashboard/reports/incomes/incomes.component.ts
+++ src/app/pages/dashboard/reports/incomes/incomes.component.ts
@@ -2,6 +2,7 @@ import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
 import {
   AfterViewInit,
   Component,
+  ElementRef,
   inject,
   OnInit,
   signal,
@@ -19,8 +20,10 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { merge, startWith } from 'rxjs';
 import { endOfDay, startOfDay } from 'date-fns';
-import { ReportsData, ReportsService } from '@services';
+import { ReportsData, ReportsGrouped, ReportsService } from '@services';
 import { FolioPipe, MethodPipe } from '@pipes';
+import { init } from 'echarts';
+import { PaymentMethod } from '@graphql';
 
 @Component({
   selector: 'app-incomes',
@@ -44,11 +47,17 @@ import { FolioPipe, MethodPipe } from '@pipes';
   templateUrl: './incomes.component.html',
   styles: ``,
 })
-export class IncomesComponent implements OnInit, AfterViewInit {
+export class IncomesComponent implements AfterViewInit {
   @ViewChild('paginator') public paginator!: MatPaginator;
+  @ViewChild('branchsChart')
+  public branchsChartElement!: ElementRef<HTMLDivElement>;
+  @ViewChild('methodsChart')
+  public methodsChartElement!: ElementRef<HTMLDivElement>;
 
   public loading = signal<boolean>(false);
   public total = signal<number>(0);
+  public incomeBranchs = signal<ReportsGrouped[]>([]);
+  public incomeMethods = signal<ReportsGrouped[]>([]);
   public displayedColumns: string[] = [
     'branchName',
     'studentNames',
@@ -66,7 +75,9 @@ export class IncomesComponent implements OnInit, AfterViewInit {
   public startDateControl = new FormControl<Date>(startOfDay(new Date()));
   public endDateControl = new FormControl<Date>(endOfDay(new Date()));
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+
     merge(this.startDateControl.valueChanges, this.endDateControl.valueChanges)
       .pipe(startWith(null))
       .subscribe({
@@ -74,10 +85,6 @@ export class IncomesComponent implements OnInit, AfterViewInit {
           this.refresh();
         },
       });
-  }
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
   }
 
   public refresh() {
@@ -90,13 +97,117 @@ export class IncomesComponent implements OnInit, AfterViewInit {
       this.reportsService.incomes(start, end).subscribe({
         next: (response) => {
           this.dataSource.data = response.data;
+          this.incomeBranchs.set(response.groupedByBranch);
+          this.incomeMethods.set(response.groupedByMethod);
           this.total.set(response.total);
           this.loading.set(false);
+          this.drawCharts();
         },
         error: (error) => {
           console.error('Error fetching incomes:', error);
           this.loading.set(false);
         },
+      });
+    }
+  }
+
+  private drawCharts() {
+    if (this.branchsChartElement.nativeElement) {
+      const branchsChart = init(this.branchsChartElement.nativeElement);
+
+      const branchsData = this.incomeBranchs().map((grouped) => ({
+        value: grouped.count,
+        name: grouped.name,
+      }));
+
+      branchsChart.setOption({
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          top: '5%',
+          left: 'center',
+        },
+        series: [
+          {
+            name: 'Ingresos por sucursal',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            padAngle: 5,
+            itemStyle: {
+              borderRadius: 10,
+            },
+            label: {
+              show: false,
+              position: 'center',
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 24,
+                fontWeight: 'bold',
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: branchsData,
+          },
+        ],
+      });
+    }
+
+    if (this.methodsChartElement.nativeElement) {
+      const methodsChart = init(this.methodsChartElement.nativeElement);
+
+      const paymentNames: any = {
+        [PaymentMethod.Card]: 'Tarjeta',
+        [PaymentMethod.Cash]: 'Efectivo',
+        [PaymentMethod.Transfer]: 'Transferencia',
+        [PaymentMethod.Clip]: 'Clip',
+      };
+
+      const methodsData = this.incomeMethods().map((grouped) => ({
+        value: grouped.count,
+        name: paymentNames[grouped.id.toUpperCase()],
+      }));
+
+      methodsChart.setOption({
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          top: '5%',
+          left: 'center',
+        },
+        series: [
+          {
+            name: 'Ingresos por método',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            padAngle: 5,
+            itemStyle: {
+              borderRadius: 10,
+            },
+            label: {
+              show: false,
+              position: 'center',
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 24,
+                fontWeight: 'bold',
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: methodsData,
+          },
+        ],
       });
     }
   }
